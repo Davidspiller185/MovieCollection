14c610b96454a9e0c687e21e3eceadfafd936ca5
david <dspiller321@gmail.com>
2026-07-03 14:06:50 +0300

    add file of validor

---
+++ utils/validator.js
@@ -0,0 +1,12 @@
+export function validName(name){
+    return typeof name === "string" && name.trim() !== ""
+}
+
+export function validYear(year){
+    return  typeof year === "number" && year > 1900 && year <= new Date().getFullYear()
+}
+
+export function validRating(rating){
+    return !isNaN(rating) && rating >=0 && rating <=10
+}
+
