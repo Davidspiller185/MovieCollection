8bc41999a335610c6e9ec0d6cb65d0afd57e2182
david <dspiller321@gmail.com>
2026-07-03 13:39:02 +0300

    add function of movie.servise.js

--- service/movie.service.js
+++ service/movie.service.js
@@ -1,11 +1,112 @@
-import { readFileMovies, writeFileMovies } from "./file.service";
+import { appendFileMovies, readFileMovies, writeFileMovies } from "./file.service.js";
 
-export async function showAllMovies(data) {
-    let movies = []
-    for (let i of data) {
-        movies.push(i[title])
+export  async function showAllMoviesTitle() {
+    try{
+        const data =await readFileMovies()
+        let movies = []
+        for (let movi of data) {
+        movies.push(movi["title"])
+        }
+        return movies
+    }catch(err){
+        return err.message
     }
-    return await movies
+
 }
 
-readFileMovies(showAllMovies())
+export async function ShowById(id) {
+    try{
+        const data = await readFileMovies()
+        const Find =data.find(movi => movi.id === id)
+        if (Find){
+            return Find
+        }
+        return "id not found"
+    }
+    catch(err){
+        return err.message
+    }
+}
+
+export async function CreateNewMovie(title,genre,year,rating) {
+    try{
+        let id = 0
+        const data = await readFileMovies()
+        if (data.length === 0){
+            id += 1
+        }
+        else{
+             id = data[data.length-1].id + 1
+        }
+        const obj = {id,title,genre,year,rating}
+        data.push(obj)
+        await writeFileMovies(data)
+        return "succsses to create a new movie"
+    }
+    catch(err){
+        return err.message
+    }
+}
+export async function DeleteMovie(id) {
+        try{
+            const data = await readFileMovies()
+            const Filter = data.filter(movi => movi.id !== id)
+            await writeFileMovies(Filter)
+            return "movie deleted"
+        }
+        catch(err){
+            return err.message
+        }
+}
+
+export async function UpdateRate(id,newRate) {
+    try{
+        const data = await readFileMovies()
+        const Map = data.map(movi => {if (movi.id === id); movi.rating = newRate})
+        await writeFileMovies(Map)
+        return "succsses updateRate"
+    }
+    catch(err){
+        return err.message
+    }
+}
+
+export async function SearchByName(str) {
+    try{
+        const data = await readFileMovies()
+        const Filter = data.filter(movi => movi.title.toLowerCase().includes(str.toLowerCase()))
+        return Filter
+
+    }
+    catch(err){
+        return err.message
+    }
+}
+
+export async function SortByGenres(genre) {
+    try{
+        const data = await readFileMovies()
+        const Filter = data.filter(movi =>movi.genre === genre)
+        return Filter
+    }
+    catch(err){
+        return err.message
+    }
+}
+
+export async function ShowStatistics(params) {
+    try{
+        const data = await readFileMovies()
+        console.log(`number of movi is: ${data.length}`)
+        const sumRating = data.reduce((acc,current) => acc + current.rating,0)
+        const averageRating = sumRating/average
+        console.log(`averageRating is: ${averageRating}`)
+        const maxRating = data.reduce((acc, current) =>{if (current.rating > acc) return current.rating; return acc},data[0].rating)
+        
+    }catch(err){
+        return err.message
+
+    }
+
+}
+
